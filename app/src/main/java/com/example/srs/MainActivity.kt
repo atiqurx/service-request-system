8fac40fe19049b2ef1143363dc3b9588d55c7599
Talha Tahmid <talhathmd@gmail.com>
2024-04-07 16:44:26 -0500

    Changed main page to getstarted

--- app/src/main/java/com/example/srs/MainActivity.kt
+++ app/src/main/java/com/example/srs/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.srs
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
