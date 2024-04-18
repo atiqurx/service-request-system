5019426f8417d29e2b5df4316d0ba6ecef9acecd
Atiqur Rahman <atiqur.main@gmail.com>
2024-04-17 20:58:37 -0500

    add bottom nav bar with home, services and profile

--- app/src/main/java/com/example/srs/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/srs/ui/profile/ProfileFragment.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.srs.databinding.FragmentProfileBinding;
+import com.example.srs.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
