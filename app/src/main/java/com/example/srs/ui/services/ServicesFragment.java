410aa8cd1381cae90627f5442f7a58fc0920857b
Atiqur Rahman <atiqur.main@gmail.com>
2024-04-18 14:13:59 -0500

    add services to nav

--- app/src/main/java/com/example/srs/ui/services/ServicesFragment.java
+++ app/src/main/java/com/example/srs/ui/services/ServicesFragment.java
@@ -1,4 +1,4 @@
-package com.example.srs.ui.dashboard;
+package com.example.srs.ui.services;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.srs.databinding.FragmentDashboardBinding;
+import com.example.srs.databinding.FragmentServicesBinding;
 
-public class DashboardFragment extends Fragment {
+public class ServicesFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentServicesBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        ServicesViewModel servicesViewModel =
+                new ViewModelProvider(this).get(ServicesViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentServicesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textServices;
+        servicesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/srs/ui/services/ServicesViewModel.java
+++ app/src/main/java/com/example/srs/ui/services/ServicesViewModel.java
@@ -1,16 +1,15 @@
-package com.example.srs.ui.dashboard;
+package com.example.srs.ui.services;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ServicesViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public ServicesViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
     }
 
     public LiveData<String> getText() {
