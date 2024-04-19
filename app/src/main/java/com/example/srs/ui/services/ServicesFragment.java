9d3ad8b023ef6050b37fa3072b9b7b98d7e0c0fe
Atiqur Rahman <atiqur.main@gmail.com>
2024-04-18 20:34:15 -0500

    link service categories to search results

--- app/src/main/java/com/example/srs/ui/services/ServicesFragment.java
+++ app/src/main/java/com/example/srs/ui/services/ServicesFragment.java
@@ -5,11 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.GridLayout;
+import com.example.srs.R;
+import android.widget.Button;
+import android.content.Intent;
+
+
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.srs.SearchResults;
 import com.example.srs.databinding.FragmentServicesBinding;
 
 public class ServicesFragment extends Fragment {
@@ -26,6 +33,21 @@ public class ServicesFragment extends Fragment {
 
         final TextView textView = binding.textServices;
         servicesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        // Find all buttons in the GridLayout
+        GridLayout buttonGridLayout = root.findViewById(R.id.buttonGridLayout);
+        for (int i = 0; i < buttonGridLayout.getChildCount(); i++) {
+            View view = buttonGridLayout.getChildAt(i);
+            if (view instanceof Button) {
+                Button button = (Button) view;
+                button.setOnClickListener(v -> {
+                    // Handle button click
+                    Intent intent = new Intent(getActivity(), SearchResults.class);
+                    startActivity(intent);
+                });
+            }
+        }
+
         return root;
     }
 
